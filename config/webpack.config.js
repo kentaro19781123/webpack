54d4ca95132022a206ceda4b10b868c6661ed43c
suzuki kentaro <suzuki-ke@gnavi.co.jp>
2019-10-01 17:31:58 +0900

    html-webpack-pluginの設定変更

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const path = require('path')
+const globule = require('globule')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
@@ -6,14 +7,31 @@ const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries')
 
-module.exports = {
+var rootPath = path.resolve(__dirname, '../')
+const targetTypes = { pug : 'html'}
+
+const entriesList = {};
+const getEntriesList = () => {
+  for(const [ srcType, targetType ] of Object.entries(targetTypes)) {
+    const filesMatched = globule.find([`**/*.${srcType}`, `!**/_*.${srcType}`], { cwd : `${rootPath}/src` });
+
+    for(const srcName of filesMatched) {
+      const targetPathName = srcName.replace(new RegExp(`.${srcType}$`, 'i'), `.${targetType}`);
+      const targetName = targetPathName.replace('page/', '');
+      entriesList[targetName] = `${rootPath}/src/${srcName}`;
+    }
+  }
+  return entriesList;
+}
+
+const config = {
   entry: {
     index: './src/js/index.js',
     main: './src/css/style.css'
   },
   output: {
     filename: 'js/[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '../dist')
   },
   module: {
     rules: [
@@ -80,14 +98,6 @@ module.exports = {
       from: './src/img',
       to: 'img'
     }]),
-    new HtmlWebPackPlugin({
-      template: './src/page/index.pug',
-      filename: './index.html'
-    }),
-    new HtmlWebPackPlugin({
-      template: './src/page/content/index.pug',
-      filename: './content/index.html'
-    }),
     new FixStyleOnlyEntriesPlugin(),
     new MiniCssExtractPlugin({
       filename: './css/[name].css'
@@ -97,3 +107,12 @@ module.exports = {
     minimizer: [new OptimizeCSSAssetsPlugin({})]
   }
 }
+
+for(const [ targetName, srcName ] of Object.entries(getEntriesList())) {
+  config.plugins.push(new HtmlWebPackPlugin({
+    filename : targetName,
+    template : srcName
+  }));
+}
+
+module.exports = config
